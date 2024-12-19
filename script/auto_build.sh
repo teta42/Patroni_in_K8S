2353f0fa8602b74542b4051ffa666b1b03854b53
teta <covo@net.net>
2024-12-19 15:10:16 +0500

    cataloguing

--- script/auto_build.sh
+++ script/auto_build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 docker login
 
-cd patroni
+cd ../patroni
 
 docker build -t teta42/patroni -f patroni.dockerfile .
 docker push teta42/patroni
--- script/deploy.sh
+++ script/deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+cd ../manifest
+
 kubectl apply -f etcd.yaml
 kubectl apply -f patroni.yaml
